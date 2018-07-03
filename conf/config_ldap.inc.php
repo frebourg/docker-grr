2179dafdd0c537e8965ab7a7a4f1ae4945457b62
FREBOURG Jean-Francois <Jean-Francois.frebourg@selhagroup.com>
2018-07-03 12:49:54 +0200

    update

--- conf/config_ldap.inc.php
+++ conf/config_ldap.inc.php
@@ -2,15 +2,15 @@
 # Les quatre lignes suivantes sont à modifier selon votre configuration
 # ligne suivante : l'adresse de l'annuaire LDAP.
 # Si c'est le même que celui qui heberge les scripts, mettre "localhost"
-$ldap_adresse='einea_infra01.domselha.fr';
+$ldap_adresse='';
 # ligne suivante : le port utilisé
 $ldap_port='389';
 # ligne suivante : l'identifiant et le mot de passe dans le cas d'un accès non anonyme
-$ldap_login='LDAPREAD';
+$ldap_login='';
 # Remarque : des problèmes liés à un mot de passe contenant un ou plusieurs caractères accentués ont déjà été constatés.
-$ldap_pwd='ldapread05';
+$ldap_pwd='';
 # ligne suivante : le chemin d'accès dans l'annuaire
-$ldap_base='OU=SELHAGROUP,DC=domselha,DC=fr';
+$ldap_base='';
 # ligne suivante : filtre LDAP supplémentaire (facultatif)
 $ldap_filter='';
 # ligne suivante : utiliser TLS
